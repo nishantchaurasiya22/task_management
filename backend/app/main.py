fd537917a391d2c36ab94713e9113b8c3b09ec55
nishantchaurasiya22 <25f2008456@ds.study.iitm.ac.in>
2026-08-20 14:11:16 +0530

    fix cors for vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,7 @@ app=FastAPI(title="TASK_MANAGER_API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=["https://task-management-frontend-rho-two.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
